faf9d97bbb01241d1f8ca11df6d164fd9180925a
Thor Anderson <ta.anderson@gmail.com>
2021-10-21 12:41:35 -0600

    Did some filtering & getLastNumber

--- codejournal.js
+++ codejournal.js
@@ -87,3 +87,42 @@ element => {
   return console.log(element) // explicit 'return' because of multiple lines
 }
 (num1, num2) => num1 + num2
+
+// basic syntax is num => 'The Num'
+
+// a higher order function is a function that accepts another function as a parameter.
+// filter, map and reduce are the most popular, but forEach, every, find, and some are also HOFs
+
+const theFunction = () => {
+  //multiple lines use curly braces and 'return' keyword
+  return "I am awesome";
+};
+
+// Filter method example.  Filter returns an array of all elements that 'pass the test'
+const pilots = [
+  {
+    id: 2,
+    name: "Wedge Antilles",
+    faction: "Rebels"
+  },
+  {
+    id: 8,
+    name: "Ciena Ree",
+    faction: "Empire"
+  },
+  {
+    id: 40,
+    name: "Iden Versio",
+    faction: "Empire"
+  },
+  {
+    id: 66,
+    name: "Thane Kyrell",
+    faction: "Rebels"
+  }
+];
+
+const rebels = pilots.filter((pilot) => pilot.faction === "Rebels")
+const empire = pilots.filter((pilot) => {
+  return pilot.faction === "Empire"
+})
