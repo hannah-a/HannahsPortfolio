cdb5c7dfae8c3993065cb838606c288207da4099
Hannah Armstrong <hannahlinarmstrong@gmail.com>
2021-12-01 10:46:18 -0700

    magic the gathering

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -36,3 +36,8 @@ function populatePokeCards(singlePokemon) {
     pokeGrid.appendChild(pokeScene)
 //then put it in the DOM, need a reference for the DOM, pokeGrid. 
 }
+
+// magic the gathering API Boom baby 
+// the cat link https://api.scryfall.com/cards/search?as=grid&order=name&q=%28type%3Acreature+type%3Acat%29&format=json
+
+//rare cat 62 objects https://api.scryfall.com/cards/search?as=grid&order=name&q=type%3Acat+rarity%3Ar&format=json
