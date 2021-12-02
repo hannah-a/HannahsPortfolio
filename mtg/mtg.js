4355b9d1b423b07ef36fb3c887a9e9ac8281e7de
Hannah Armstrong <hannahlinarmstrong@gmail.com>
2021-12-02 12:26:28 -0700

    uptodate

---
+++ mtg/mtg.js
@@ -0,0 +1,4 @@
+// magic the gathering API Boom baby 
+// the cat link https://api.scryfall.com/cards/search?as=grid&order=name&q=%28type%3Acreature+type%3Acat%29&format=json
+
+//rare cat 62 objects https://api.scryfall.com/cards/search?as=grid&order=name&q=type%3Acat+rarity%3Ar&format=json
