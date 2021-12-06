a3c480630153a3db5500e65f9af5c6e528781576
Hannah Armstrong <hannahlinarmstrong@gmail.com>
2021-12-05 17:36:09 -0700

    working card backs!!!

--- mtg/mtg.js
+++ mtg/mtg.js
@@ -1,9 +1,16 @@
 // magic the gathering API Boom baby 
 // the cat link https://api.scryfall.com/cards/search?as=grid&order=name&q=%28type%3Acreature+type%3Acat%29&format=json
 
+import { removeChildren } from "../utils/index.js";
+
 //rare cat 62 objects https://api.scryfall.com/cards/search?as=grid&order=name&q=type%3Acat+rarity%3Ar&format=json
 
-
+const mtgGrid = document.querySelector(".mtgGrid");
+const loadButton = document.querySelector(".loadCats")
+loadButton.addEventListener('click', () => {
+  removeChildren(mtgGrid)
+  loadMTG()
+})
 
 function getAPIData(url) { //removing async makes data come in order
     try {
@@ -18,10 +25,50 @@ function getAPIData(url) { //removing async makes data come in order
         `https://api.scryfall.com/cards/search?as=grid&order=name&q=type%3Acat+rarity%3Ar&format=json`
     ).then((mtgData) => {
       for (const card of mtgData.data) { //inside for loop, just want the results array, will give each pokemon one at a time
-        console.log(card.name, card.image_uris.normal) //if we have one pokemon at a time, we can populate each card for each one
+       populateCard(card) //I already have the data so I don't think I need to get the data from the url
       }
     })
   }
   loadMTG()
+function populateCard(singleCard) {
+  const mtgScene = document.createElement('div')
+  mtgScene.className = 'scene'
+  const mtgCard = document.createElement('div')
+  mtgCard.className = 'card'
+  mtgCard.addEventListener('click', () => mtgCard.classList.toggle('is-flipped'))
 
+  const front = populateCardFront(singleCard)
+  const back = populateCardBack(singleCard)
+
+  mtgCard.appendChild(front)
+  mtgCard.appendChild(back)
+  mtgScene.appendChild(mtgCard)
+  mtgGrid.appendChild(mtgScene)
+}
+
+function populateCardFront(card) {
+  const mtgFront = document.createElement('figure')
+  mtgFront.className = 'cardFace front'
+ const imgFront = document.createElement('img')
+ imgFront.src = '../images/magiccardback.jpg/'
+
+  const mtgCaption = document.createElement('figcaption')
+  mtgCaption.textContent = card.name
+  mtgFront.appendChild(imgFront)
+  mtgFront.appendChild(mtgCaption)
+
+  return mtgFront
+}
+
+function populateCardBack(mtgcard) {
+  const mtgBack = document.createElement('figure')
+  mtgBack.className = 'cardFace back'
+  const imgBack = document.createElement('img')
+  imgBack.src = mtgcard.image_uris.normal
+
+  mtgBack.appendChild(imgBack)
+ 
+
+  return mtgBack
+}
   //tomorrow, make css card, populate card back and fronts, research pagination, make random button
--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,171 +1,203 @@
-import { removeChildren } from '../utils/index.js'
+import { removeChildren } from "../utils/index.js";
 
-function getAPIData(url) { //removing async makes data come in order
+function getAPIData(url) {
+  //removing async makes data come in order
   try {
-    return fetch(url).then((data) => data.json()) //then with data it will data in json by calling it, it's going to call the api ten times inorder
+    return fetch(url).then((data) => data.json()); //then with data it will data in json by calling it, it's going to call the api ten times inorder
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
-function loadPokemon(offset = 0, limit = 25) { //without a function, js will just read it and call it but with a button you can control it so put it in function
+function loadPokemon(offset = 0, limit = 25) {
+  //without a function, js will just read it and call it but with a button you can control it so put it in function
   getAPIData(
-    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`,
-  ).then(async /* in second api data call*/(data) => {
-    for (const pokemon of data.results) { //inside for loop, just want the results array, will give each pokemon one at a time
-      await /*await this function forcing it to go in order of our for loop*/ getAPIData(pokemon.url).then((pokeData) => //only two properties in the object, can call getApi data on each url property
-        populatePokeCard(pokeData/*populate using pokeData*/), //if we have one pokemon at a time, we can populate each card for each one
-      )
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then(async (/* in second api data call*/ data) => {
+    for (const pokemon of data.results) {
+      //inside for loop, just want the results array, will give each pokemon one at a time
+      await /*await this function forcing it to go in order of our for loop*/ getAPIData( //this would be useful to create another page of more mtg cards by using the uri
+        pokemon.url
+      ).then(
+        (
+          pokeData //only two properties in the object, can call getApi data on each url property
+        ) => populatePokeCard(pokeData /*populate using pokeData*/) //if we have one pokemon at a time, we can populate each card for each one
+      );
     }
-  })
+  });
 }
 
-const pokeGrid = document.querySelector('.pokeGrid')
-const loadButton = document.querySelector('.loadPokemon')
-loadButton.addEventListener('click', () => {
-  removeChildren(pokeGrid)
-  loadPokemon(100, 5)
-})
 
-const moreButton = document.querySelector('.morePokemon')
-moreButton.addEventListener('click', () => {
-  let limit = prompt('How many more Pokemon should I load?')
-  let offset = prompt('At which Pokemon ID should I start loading?')
-  loadPokemon(offset, limit)
-})
 
-const newButton = document.querySelector('.newPokemon')
-newButton.addEventListener('click', () => {
-  let pokeName = prompt('What is the name of your new Pokemon?')
-  let pokeHeight = prompt("What is the Pokemon's height?")
-  let pokeWeight = prompt("What is the Pokemon's weight?")
+
+
+
+// Buttons
+
+const pokeGrid = document.querySelector(".pokeGrid");
+const loadButton = document.querySelector(".loadPokemon");
+loadButton.addEventListener("click", () => {
+  removeChildren(pokeGrid);
+  loadPokemon(100, 5); // arrow function returns the calling of the loadPokemon function
+});
+
+const moreButton = document.querySelector(".morePokemon");
+moreButton.addEventListener("click", () => {
+  let limit = prompt("How many more Pokemon should I load?");
+  let offset = prompt("At which Pokemon ID should I start loading?");
+  loadPokemon(offset, limit);
+});
+
+const newButton = document.querySelector(".newPokemon");
+newButton.addEventListener("click", () => {
+  let pokeName = prompt("What is the name of your new Pokemon?");
+  let pokeHeight = prompt("What is the Pokemon's height?");
+  let pokeWeight = prompt("What is the Pokemon's weight?");
   let pokeAbilities = prompt(
-    'What are your Pokemon abilities? (use a comma separated list)',
-  )
+    "What are your Pokemon abilities? (use a comma separated list)"
+  );
   let newPokemon = new Pokemon(
     pokeName,
     pokeHeight,
     pokeWeight,
-    getAbilitiesArray(pokeAbilities),
-  )
-  populatePokeCard(newPokemon)
-})
+    getAbilitiesArray(pokeAbilities)
+  );
+  populatePokeCard(newPokemon);
+});
+
+
+
+// New Button stuff
 
 function getAbilitiesArray(commaString) {
-  let tempArray = commaString.split(',')
+  let tempArray = commaString.split(","); //split method divides strings into an array searching for the pattern in the first parameter
   return tempArray.map((abilityName) => {
     return {
       ability: {
-        name: abilityName,
+        name: abilityName, //returns an object with the first parameter as the value
       },
-    }
-  })
+    };
+  });
 }
 
 class Pokemon {
-  constructor(name, height, weight, abilities) {
-    ;(this.id = 100),
+  constructor(
+    name,
+    height,
+    weight,
+    abilities
+  ) /*pass in properties you want it to have */ {
+    (this.id = 100),
       (this.name = name),
       (this.height = height),
       (this.weight = weight),
-      (this.abilities = abilities)
+      (this.abilities = abilities);
   }
 }
 
+
+
+
+
+
+// Populate Function Cards
+
+
 function populatePokeCard(singlePokemon) {
-  const pokeScene = document.createElement('div')
-  pokeScene.className = 'scene'
-  const pokeCard = document.createElement('div')
-  pokeCard.className = 'card'
-  pokeCard.addEventListener('click', () =>
-    pokeCard.classList.toggle('is-flipped'),
-  ) // added event listener to make a button to tranform the card
+  const pokeScene = document.createElement("div");
+  pokeScene.className = "scene";
+  const pokeCard = document.createElement("div");
+  pokeCard.className = "card";
+  pokeCard.addEventListener("click", () =>
+    pokeCard.classList.toggle("is-flipped")
+  ); // added event listener to make a button to tranform the card
 
-  const front = populateCardFront(singlePokemon) //will call function, will pass single pokemon to this function
-  const back = populateCardBack(singlePokemon)
+  const front = populateCardFront(singlePokemon); //will call function, will pass single pokemon to this function
+  const back = populateCardBack(singlePokemon);
 
-  pokeCard.appendChild(front)
-  pokeCard.appendChild(back)
-  pokeScene.appendChild(pokeCard)
-  pokeGrid.appendChild(pokeScene)
+  pokeCard.appendChild(front);
+  pokeCard.appendChild(back);
+  pokeScene.appendChild(pokeCard);
+  pokeGrid.appendChild(pokeScene);
 }
- //these two functions to bring in the front card
-function populateCardFront(pokemon) /*takes single pokemon */{
-  const pokeFront = document.createElement('figure')
-  pokeFront.className = 'cardFace front'       
-  const pokeImg = document.createElement('img')
-  pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+//these two functions to bring in the front card
+function populateCardFront(pokemon) /*takes single pokemon */ {
+  const pokeFront = document.createElement("figure");
+  pokeFront.className = "cardFace front";
+  const pokeImg = document.createElement("img");
+  pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
 
-  const pokeCaption = document.createElement('figcaption')
-  pokeCaption.textContent = pokemon.name
-  pokeFront.appendChild(pokeImg)
-  pokeFront.appendChild(pokeCaption)
+  const pokeCaption = document.createElement("figcaption");
+  pokeCaption.textContent = pokemon.name;
+  pokeFront.appendChild(pokeImg);
+  pokeFront.appendChild(pokeCaption);
 
   //typesBackground(pokemon, pokeFront)
-  return pokeFront //need to return it so that front equals
+  return pokeFront; //need to return it so that front equals
 }
 
 //could use populate back to populate the card image. I could use the card image and really this because I wouldn't need to add the information myself instead I could just use the images and then have a search button that will filter, map and reduce the data to find certain cards.
 function populateCardBack(pokemon) {
-  const pokeBack = document.createElement('div')
-  pokeBack.className = 'cardFace back'
-  const label = document.createElement('h4')
-  label.textContent = 'Abilities:' //created an h4 text
-  pokeBack.appendChild(label)
-  const abilityList = document.createElement('ul') //don't want to numbers to show up
-  pokemon.abilities.forEach((abilityItem) => { //name of property is abilities
-    let listItem = document.createElement('li')
-    listItem.textContent = abilityItem.ability.name
-    abilityList.appendChild(listItem)
-  })
-  pokeBack.appendChild(abilityList)
-  return pokeBack
+  const pokeBack = document.createElement("div");
+  pokeBack.className = "cardFace back";
+  const label = document.createElement("h4");
+  label.textContent = "Abilities:"; //created an h4 text
+  pokeBack.appendChild(label);
+  const abilityList = document.createElement("ul"); //don't want to numbers to show up
+  pokemon.abilities.forEach((abilityItem) => {
+    //name of property is abilities
+    let listItem = document.createElement("li");
+    listItem.textContent = abilityItem.ability.name;
+    abilityList.appendChild(listItem);
+  });
+  pokeBack.appendChild(abilityList);
+  return pokeBack;
 }
+
 
 
 
 function typesBackground(pokemon, card) {
-  let pokeType1 = pokemon.types[0].type.name
-  let pokeType2 = pokemon.types[1]?.type.name
-  console.log(pokeType1, pokeType2)
+  let pokeType1 = pokemon.types[0].type.name;
+  let pokeType2 = pokemon.types[1]?.type.name;
+  console.log(pokeType1, pokeType2);
   card.style.setProperty(
-    'background',
-    `linear-gradient(${getPokeTypeColor(pokeType1)}, #FFF})`,
-  )
+    "background",
+    `linear-gradient(${getPokeTypeColor(pokeType1)}, #FFF})`
+  );
 }
 
 function getPokeTypeColor(pokeType) {
-  let color
+  let color;
   switch (pokeType) {
-    case 'grass':
-      color = '#00FF00'
-      break
-    case 'fire':
-      color = '#FF0000'
-      break
-    case 'water':
-      color = '#0000FF'
-      break
-    case 'bug':
-      color = '#7FFF00'
-      break
-    case 'normal':
-      color = '#F5F5DC'
-      break
-    case 'flying':
-      color = '#00FFFF'
-      break
-    case 'poison':
-      color = '#C300FF'
-      break
-    case 'electric':
-      color = '#C8FF00'
-      break
-      case 'psychic':
-        color = '#333333'
-        break
+    case "grass":
+      color = "#00FF00";
+      break;
+    case "fire":
+      color = "#FF0000";
+      break;
+    case "water":
+      color = "#0000FF";
+      break;
+    case "bug":
+      color = "#7FFF00";
+      break;
+    case "normal":
+      color = "#F5F5DC";
+      break;
+    case "flying":
+      color = "#00FFFF";
+      break;
+    case "poison":
+      color = "#C300FF";
+      break;
+    case "electric":
+      color = "#C8FF00";
+      break;
+    case "psychic":
+      color = "#333333";
+      break;
     default:
-      color = '#888888'
+      color = "#888888";
   }
 }
-
